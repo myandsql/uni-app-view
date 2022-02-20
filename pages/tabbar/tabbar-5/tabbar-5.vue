58bab415b272de180668061935f3085a9d00fc4f
myandsql <2460713358@qq.com>
2022-02-20 15:54:15 +0800

    添加项目

---
+++ pages/tabbar/tabbar-5/tabbar-5.vue
@@ -0,0 +1,31 @@
+<template>
+	<view >
+			<u-button> 1</u-button>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				src:'static/img/boy.png'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+		margin-top: 200upx;
+	}
+</style>
